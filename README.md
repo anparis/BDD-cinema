ddae65105bfdea2a2853553cc8bfd880546824c0
Antonin Paris <antonin.pariss@gmail.com>
2022-10-27 00:17:59 +0200

    première version du MCD et du script sql correspondant

---
+++ README.md
@@ -0,0 +1,6 @@
+# Exercice Cinéma
+But : Construire une BDD à partir d'un MCD et afficher ses données avec php.
+
+## Mon MCD
+| réaliser avec JMerise
+![mcd base de donnée cinéma](cinema-schema.png)
---
+++ script-insert.sql
@@ -0,0 +1,23 @@
+#------------------------------------------------------------
+# Insertions
+#------------------------------------------------------------
+
+INSERT INTO Genre (libelle) VALUES
+	('science-fiction'),
+	('Fantasy'),
+	('Thriller');
+
+INSERT INTO Realisateur (nom, prenom, date_naissance) VALUES
+	('Villeneuve', 'Denis', '1967-10-03'), 
+	('Jackson', 'Peter', '1961-10-31'),
+    ('Scott', 'Ridley', '1937-11-30');
+
+INSERT INTO Acteur (nom, prenom, date_naissance) VALUES
+	('Villeneuve', 'Denis', '1967-10-03'), 
+	('Jackson', 'Peter', '1961-10-31'),
+    ('Scott', 'Ridley', '1937-11-30');
+-- FILM :
+-- Le seigneur des anneaux : la Communauté de l'anneau		
+-- Prisoners
+-- Blade Runner
+-- DUNE
